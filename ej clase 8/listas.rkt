08dcd66df2e53caf1d701aa595523ba62f662f16
manolo <manolosavi@me.com>
2016-02-12 11:48:13 -0600

    ej 8: 2a & 2b

---
+++ ej clase 8/listas.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+; mayor
+(define (mayor l)
+  (m2 (cdr l) (car l))
+)
+
+(define (m2 l max)
+  (cond 
+    ((eq? l '())
+      max)
+    ((< (car l) max)
+      (m2 (cdr l) max))
+    (else (m2 (cdr l) (car l)))
+  )
+)
+
+; secuencia
+(define (secuencia n)
+  (s2 n 0)
+)
+
+(define (s2 n x)
+  (if (= n x) (cons n '())
+      (cons x (s2 n (+ x 1)))
+  )
+)
+
+; elimina
+(define (elimina l)
+  
+)
